f80bc3b1a843fc75c714ceb3d9a533b85a82f707
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2023-12-22 22:56:45 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./router.jsx";
 import "./index.css";
